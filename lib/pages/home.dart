c0d7011434f81d59ffe12e719208bbe1147a1b5a
dozie-oc <chidozieoriaku@gmail.com>
2026-07-15 16:42:41 +0100

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Dinner',
+        style : TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          ),
+      ),
+      backgroundColor: Colors.orange,
+      centerTitle: true,
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(50, 255, 255, 255),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 30,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(50, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset('assets/icons/dots.svg',
+          height: 5,
+          width: 5,
+        )
+        )
+      ],
+      ),
+    );
+  }
+}
